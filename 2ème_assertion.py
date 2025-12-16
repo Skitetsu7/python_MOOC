6b4b5132c717479bd293512df18de1dfce3a339b
Mohamed Boukhatem <boukhatemmohamed121@gmail.com>
2025-12-16 10:13:56 +0100

    Add pytest output for test_assertion.py

---
+++ 2ème_assertion.py
@@ -0,0 +1,10 @@
+PS C:\Users\El familia\OneDrive\mp3> python -m pytest test_assertion.py
+=============== test session starts ===============
+platform win32 -- Python 3.9.0, pytest-8.4.2, pluggy-1.6.0
+rootdir: C:\Users\El familia\OneDrive\mp3
+collected 5 items                                  
+
+test_assertion.py .....                      [100%]
+
+================ 5 passed in 0.04s ================ 
+PS C:\Users\El familia\OneDrive\mp3> 
